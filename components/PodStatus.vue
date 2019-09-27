b4b9f098abb6955c5cb49da4ca728760442f779e
lrlarson <lrlarson@mac.com>
2019-09-27 16:28:06 -0400

    finish pod status
    Signed-off-by: lrlarson <lrlarson@mac.com>

--- components/PodStatus.vue
+++ components/PodStatus.vue
@@ -13,25 +13,59 @@
       </div>
       <div class="col-9">Pod Story Status - click to edit
         <br />
-
+         <div>
+           <button @click="showNewEditor">Add New Pod Story</button>
+         </div>
         <div id="editor" v-if="showEditor" >
-          <div class="col-7">
+          <div class="col-10">
             <select v-model="selectedPodStory.DISP" >
-                <option v-for="disp in disciplines" v-bind:value="disp.VALUE">
+              <option v-for="disp in disciplines" v-bind:value="disp.VALUE">
                 {{ disp.LABEL }}
               </option>
             </select>
-            <input v-model="selectedPodStory.TITLE">
-            <label >Title</label>
+            <input v-model="selectedPodStory.TITLE" style="min-width: 250px;">
+            <label>Copy #</label>
+            <select v-model="selectedPodStory.COPYNUMBER" >
+              <option v-for="number in copyNumbers" v-bind:value="number">
+                {{ number }}
+              </option>
+            </select>
+
             <input type="checkbox" v-model="selectedPodStory.EVENTINDB">
             <label >In DB</label>
-            <input type="checkbox" v-model="selectedPodStory.EVENTINDB">
+            <input type="checkbox" v-model="selectedPodStory.HAVEIMAGE">
             <label >Have Image</label>
             <input type="checkbox" v-model="selectedPodStory.PROCESSED">
             <label >Set to Complete</label>
+            <button @click="editPodStatus">Update Status</button>
           </div>
 
-      </div>
+        </div>
+        <div id="editorNew" v-if="showEditorNew" >
+          <div class="col-10">
+            <select v-model="selectedPodStory.DISP" >
+              <option v-for="disp in disciplines" v-bind:value="disp.VALUE">
+                {{ disp.LABEL }}
+              </option>
+            </select>
+            <input v-model="selectedPodStory.TITLE" style="min-width: 250px;">
+            <label>Copy #</label>
+            <select v-model="selectedPodStory.COPYNUMBER" >
+              <option v-for="number in copyNumbers" v-bind:value="number">
+                {{ number }}
+              </option>
+            </select>
+
+            <input type="checkbox" v-model="selectedPodStory.EVENTINDB">
+            <label >In DB</label>
+            <input type="checkbox" v-model="selectedPodStory.HAVEIMAGE">
+            <label >Have Image</label>
+            <input type="checkbox" v-model="selectedPodStory.PROCESSED">
+            <label >Set to Complete</label>
+            <button @click="insertNewPodStatus">Save</button>
+          </div>
+
+        </div>
 
 
         <br/>
@@ -40,6 +74,7 @@
           <tr>
             <th scope="col">Disp.</th>
             <th scope="col">Title</th>
+            <th scope="col">Copy #</th>
             <th scope="col">Event in DB</th>
             <th scope="col">Have Image</th>
             <th scope="col">Processed</th>
@@ -50,6 +85,7 @@
           <tr v-for="event in podData" @click="rowClick(event.ID)">
             <td>{{event.DISCIPLINE}}</td>
             <td>{{event.TITLE}}</td>
+            <td>{{event.COPYNUMBER}}</td>
             <td>{{event.EVENTINDB}}</td>
             <td>{{event.HAVEIMAGE}}</td>
             <td>{{event.PROCESSED}}</td>
@@ -77,7 +113,9 @@
                tempPodData:[],
                showEditor: false,
                selectedPodStory:{},
-               disciplines:[]
+               disciplines:[],
+               showEditorNew: false,
+               copyNumbers:[1,2,3,4,5,6,7,8,9,10,11,13]
            }
         },
         methods:{
@@ -105,6 +143,7 @@
                 this.$parent.selectedComponent = 'SubmittedEvents';
             },
             rowClick(id){
+                this.showEditorNew = false;
                 this.showEditor = true;
                 this.upDatePodStatus(id);
             },
@@ -115,6 +154,32 @@
                         vm.disciplines = response.data;
                     })
             },
+            editPodStatus:function () {
+              var vm = this;
+              axios.get(dataURL + '?method=editSpecificPodStatus&ID=' + vm.selectedPodStory.ID + '&disp=' + vm.selectedPodStory.DISP + '&title=' + vm.selectedPodStory.TITLE + '&eventInDB=' +  vm.selectedPodStory.EVENTINDB + '&haveImage=' + vm.selectedPodStory.HAVEIMAGE + '&processed=' + vm.selectedPodStory.PROCESSED + '&lastEditBy=' + global.$user.name + '&copyNumber='+vm.selectedPodStory.COPYNUMBER)
+                .then(function (response) {
+                     vm.showEditor = false;
+                    vm.fetchPodStatus();
+                })
+            },
+            insertNewPodStatus:function(){
+              var vm = this;
+                axios.get(dataURL + '?method=insertNewPodStatus&disp=' + vm.selectedPodStory.DISP + '&title=' + vm.selectedPodStory.TITLE + '&eventInDB=' +  vm.selectedPodStory.EVENTINDB + '&haveImage=' + vm.selectedPodStory.HAVEIMAGE + '&processed=' + vm.selectedPodStory.PROCESSED + '&lastEditBy=' + global.$user.name + '&copyNumber='+vm.selectedPodStory.COPYNUMBER)
+                    .then(function (response) {
+                        vm.showEditorNew = false;
+                        vm.fetchPodStatus();
+                    })
+            },
+            showNewEditor:function () {
+               this.selectedPodStory.ID = 0;
+               this.selectedPodStory.DISP = null;
+               this.selectedPodStory.TITLE = '';
+               this.selectedPodStory.EVENTINDB = false;
+               this.selectedPodStory.HAVEIMAGE = false;
+               this.selectedPodStory.PROCESSED = false;
+               this.showEditor = false;
+               this.showEditorNew = true;
+            }
         },
         created() {
             this.getDisps();
